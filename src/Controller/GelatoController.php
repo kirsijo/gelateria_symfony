fee496ea8f4bbebfa95db399c34af41980c21ce9
Kirsi Jokela <kirsijo@gmail.com>
2022-05-28 17:43:08 +0300

    homepage description, some more styling

--- src/Controller/GelatoController.php
+++ src/Controller/GelatoController.php
@@ -3,7 +3,6 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
@@ -21,7 +20,7 @@ class GelatoController extends AbstractController
     {
 
         $flavours = [
-            'pistachio' => ['flavour' => 'pistachio', 'image'=>'https://www.davidlebovitz.com/wp-content/uploads/2007/09/1314787147_c9ac9d5bae_o-1.jpg', 'description' => 'Pistachio ice cream or pistachio nut ice cream is an ice cream flavor made with pistachio nuts or flavoring.' ],
+            'pistachio' => ['flavour' => 'pistachio', 'image'=>'https://www.davidlebovitz.com/wp-content/uploads/2007/09/1314787147_c9ac9d5bae_o-1.jpg', 'description' => 'Pistachio ice cream or pistachio nut ice cream is an ice cream flavor made with pistachio nuts.' ],
             'blackcurrant' => ['flavour' => 'blackcurrant', 'image' => 'https://5.imimg.com/data5/SELLER/Default/2021/2/UZ/FD/XU/110446045/black-currant-ice-cream-500x500.jpeg', 'description' => 'sweet and tart'],
         ];
 
