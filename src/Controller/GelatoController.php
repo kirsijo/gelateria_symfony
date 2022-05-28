168008d091b008136548ddfc7633a52f6273954b
Kirsi Jokela <kirsijo@gmail.com>
2022-05-28 15:55:50 +0300

    routes work, twig content shows, basic styling

--- src/Controller/GelatoController.php
+++ src/Controller/GelatoController.php
@@ -13,17 +13,24 @@ class GelatoController extends AbstractController
     #[Route('/', name: 'app_gelato')]
     public function homepage(): Response
     {
-        return new Response('Gelateria');
+        return $this->render('gelato/homepage.html.twig', ['title' => 'Gelato']);
     }
 
     #[Route('/flavours/{slug}')]
     public function flavour(string $slug = null): Response
     {
+
+        $flavours = [
+            'pistachio' => ['flavour' => 'pistachio', 'image'=>'https://www.davidlebovitz.com/wp-content/uploads/2007/09/1314787147_c9ac9d5bae_o-1.jpg', 'description' => 'Pistachio ice cream or pistachio nut ice cream is an ice cream flavor made with pistachio nuts or flavoring.' ],
+            'blackcurrant' => ['flavour' => 'blackcurrant', 'image' => 'https://5.imimg.com/data5/SELLER/Default/2021/2/UZ/FD/XU/110446045/black-currant-ice-cream-500x500.jpeg', 'description' => 'sweet and tart'],
+        ];
+
         if ($slug) {
             $flavour = 'Flavour: '.u(str_replace('-', ' ', $slug))->title(true);
+            return $this->render('gelato/singleflavour.html.twig', ['flavour' => $flavours[$slug]]);
         } else {
             $flavour = 'All flavours';
         }
-        return new Response($flavour);
+        return $this->render('gelato/flavours.html.twig', ['flavours' => $flavours]);
     }
 }
